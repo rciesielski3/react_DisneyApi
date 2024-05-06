a692c277d145fc45c64655c83973c5fd0aea286a
r.ciesielski3 <r.ciesielski3@gmail.com>
2024-05-06 06:46:18 +0200

    update useCharacterData in CharacterList

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import CharacterCard from "../CharacterCard/CharacterCard";
 import useCharacterData from "../useCharacterData";
 import { ListWrapper, LoadingMessage } from "./CharacterList.styles";
